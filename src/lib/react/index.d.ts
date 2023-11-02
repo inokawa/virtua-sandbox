e5d051bc67cdda628b55499adfab61fecffc073f
inokawa <48897392+inokawa@users.noreply.github.com>
2023-11-03 02:32:32 +0900

    wip

--- src/lib/react/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { VList } from "./VList";
-export type { VListProps, VListHandle } from "./VList";
-export { WVList } from "./WVList";
-export type { WVListProps, WVListHandle } from "./WVList";
-export { VGrid as experimental_VGrid } from "./VGrid";
-export type { VGridProps, VGridHandle, CustomCellComponent, CustomCellComponentProps, } from "./VGrid";
-export type { ViewportComponentAttributes, CustomViewportComponent, CustomViewportComponentProps, } from "./Viewport";
-export type { CustomItemComponent, CustomItemComponentProps } from "./ListItem";
--- src/lib/react/useLatestRef.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const useLatestRef: <T>(value: T) => import("react").MutableRefObject<T>;
