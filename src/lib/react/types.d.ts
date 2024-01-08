cb2141b159298154533363e9a718fdb86166701b
inokawa <48897392+inokawa@users.noreply.github.com>
2024-01-08 16:19:55 +0900

    Revert "wip"
    This reverts commit 3ca948df005dfcb0f0bacc44122164da88a298d0.

--- src/lib/react/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { CSSProperties, ReactNode } from "react";
-export type ViewportComponentAttributes = Pick<React.HTMLAttributes<HTMLElement>, "className" | "style" | "id" | "role" | "tabIndex" | "onKeyDown"> & React.AriaAttributes;
-export interface CustomContainerComponentProps {
-    style: CSSProperties;
-    children: ReactNode;
-}
-export type CustomContainerComponent = React.ForwardRefExoticComponent<React.PropsWithoutRef<CustomContainerComponentProps> & React.RefAttributes<any>>;
