607c7356643b53a7fd1cafceb3fba59780734ab7
inokawa <48897392+inokawa@users.noreply.github.com>
2024-02-10 11:43:28 +0900

    Revert "wip"
    This reverts commit 9b3270d426621a552b13512934662c5cbcc34b3d.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import {
   experimental_VGrid as VGrid,
   VGridHandle,
   WindowVirtualizer,
-} from "./lib";
+} from "virtua";
 import { useEffect, useRef, useState } from "react";
 
 const createRows = (num: number) => {
